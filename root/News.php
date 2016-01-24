158df8de94497b066e9c50b4e7cb3920e7ac6da8
SandBlock <sanderverschoor@live.nl>
2016-01-24 18:09:43 +0100

    news wip

---
+++ root/News.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <link rel="stylesheet" type="text/css" href="NewsLayout.css">
+</head>
+
+<style>
+</style>
+
+<body>
+<div class="news-module">
+<div class="news-title"><strong>Network</strong> News</div>
+<div class="news">
+    <div class="news-img"
+    </div>
+    <div class="news-article">
+    </div>
+</div>
+</div>
+</div>
+</body>
+
+</html>
