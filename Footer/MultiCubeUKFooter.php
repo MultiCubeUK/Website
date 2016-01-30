c33edeed0eb704b6af43e2d70b615b5634c1188d
SandBlock <sanderverschoor@live.nl>
2016-01-30 21:52:10 +0100

    fixed footer

--- Footer/MultiCubeUKFooter.php
+++ Footer/MultiCubeUKFooter.php
@@ -1,44 +1,36 @@
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
-<div class="multicube-footer-spacer"></div>
 <div class="multicube-footer">
-<div class="multicube-footer-container">
   <div class="multicube-inner">
       <div class="multicube-box">
       <h2>Servers</h2>
-      <ul class="multicube-sitemap">
-          <li><a href="../servers/MultiCubeUKCrundee.php">CrundeeCraft</a></li>
-          <li><a href="../servers/MultiCubeUKFlux.php">Project Flux</a></li>
-          <li><a href="../servers/MultiCubeUKMainline.php">RR3 Mainline</a></li>
-          <li><a href="../servers/MultiCubeUKRegrowth.php">FTB Regrowth</a></li>
-          <li><a href="../servers/MultiCubeUKInfinity.php">Infinity Evolved</a></li>
-          <li><a href="../servers/MultiCubeUKFactions.php">Vanilla Factions</a></li>
-          <li><a href="../servers/MultiCubeUKCreative.php">Vanilla Creative</a></li>
-          <li><a href="../servers/MultiCubeUKPixelmon.php">Pixelmon Survival</a></li>
-          <li><a href="ts3server://ts.multicube.co:4753">Join Teamspeak</a></li>
-      </ul>
+          <a href="../servers/MultiCubeUKCrundee.php">CrundeeCraft</a><br/>
+          <a href="../servers/MultiCubeUKFlux.php">Project Flux</a><br/>
+          <a href="../servers/MultiCubeUKMainline.php">RR3 Mainline</a><br/>
+          <a href="../servers/MultiCubeUKRegrowth.php">FTB Regrowth</a><br/>
+          <a href="../servers/MultiCubeUKInfinity.php">Infinity Evolved</a><br/>
+          <a href="../servers/MultiCubeUKFactions.php">Vanilla Factions</a><br/>
+          <a href="../servers/MultiCubeUKCreative.php">Vanilla Creative</a><br/>
+          <a href="../servers/MultiCubeUKPixelmon.php">Pixelmon Survival</a><br/>
+          <a href="ts3server://ts.multicube.co:4753">Join Teamspeak</a><br/>
     </div>
     <div class="multicube-box">
       <h2>Social</h2>
-      <ul class="multicube-sitemap">
-          <li><a href="https://twitter.com/multicubeuk"><i class="fa fa-twitter fa-2x"></i></a></li>
-          <li class="btn btn-block btn-social btn-twitter"></li>
-          <li><a href="https://www.youtube.com/channel/UC9DCt6T-CpbhKqLjk6fudRA"><i class="fa fa-youtube fa-2x"></i></a></li>
-          <li class="btn btn-block btn-social btn-youtube"></li>
-          <li><a href="https://www.facebook.com/MultiCubeUK/"><i class="fa fa-facebook fa-2x"></i></a></li>
-          <li class="btn btn-block btn-social btn-facebook"></li>
-      </ul>
+          <a href="https://twitter.com/multicubeuk"><i class="fa fa-twitter fa-2x"></i></a></li>
+          <p class="btn btn-block btn-social btn-twitter"></p>
+          <a href="https://www.youtube.com/channel/UC9DCt6T-CpbhKqLjk6fudRA"><i class="fa fa-youtube fa-2x"></i></a>
+          <p class="btn btn-block btn-social btn-youtube"></p>
+          <a href="https://www.facebook.com/MultiCubeUK/"><i class="fa fa-facebook fa-2x"></i></a>
+          <p class="btn btn-block btn-social btn-facebook"></p>
     </div>
     <div class="multicube-box">
       <h2>Contact</h2>
       <p><b>Phone:</b> +447521305949 Open aprox 10AM - 9PM Monday - Friday UK time. Charges may apply</p>
       <p><b>E-Mail:</b> <a href="mailto:support@multicube.co">support@multicube.co</a></p>
     </div>
-    <div class="multicube-box last">
+    <div class="multicube-box">
       <h2>Copyright</h2>
       <p>Copyright &copy; Multicube UK 2015-2016</p>
       <p>Code developed by MultiCubeUK</p>
     </div>
   </div>
-  </div>
-</div>
 </div>
