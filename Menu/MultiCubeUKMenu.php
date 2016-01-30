666aaf47552e03e8e6b404f78be234a06ac75318
SandBlock <sanderverschoor@live.nl>
2016-01-30 16:53:59 +0100

    added snow and hearts effects to menu

--- Menu/MultiCubeUKMenu.php
+++ Menu/MultiCubeUKMenu.php
@@ -1,11 +1,12 @@
 <?php echo '
-  <title>Bootstrap Case</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
   <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
   <link href="http://fonts.googleapis.com/css?family=Quicksand:400,300,700" rel="stylesheet" type="text/css">
+  <script src="../effects/snowStorm.js"></script>
+  <script src="../Menu/MultiCubeUKMenuJS.js"></script>
     
 <nav class="navbar navbar-default">
   <div class="container-fluid">
@@ -48,4 +49,35 @@
       </ul>
     </div>
   </div>
-</nav>'; ?>
+</nav>
+<img id="snowtoggle" alt="effectToggle" onclick="toggleSnow()" />
+<img id="heartstoggle" alt="effectToggle" onclick="toggleSnow()" />
+<script>
+   var d = new Date();
+   var day = d.getDate();
+   var month = d.getMonth();
+   var sbutton = document.getElementById("snowtoggle");
+   var hbutton = document.getElementById("heartstoggle");
+   
+if (day >= 1 && day <= 31 && month == 11){
+   hbutton.style.display = "none";
+   sbutton.style.display = "block";
+   snowStorm.resume();
+}else if (day >= 1 && day <= 4 && month == 0){
+   hbutton.style.display = "none";
+   sbutton.style.display = "block";
+   snowStorm.resume();
+}else if (month == 1) {
+   sbutton.style.display = "none";
+   hbutton.style.display = "block";
+   snowStorm.resume();
+   snowStorm.snowColor = "#F00000";
+   snowStorm.snowCharacter = "&hearts;";
+   snowStorm.animationInterval = 50;
+}else{
+   sbutton.style.display = "none";
+   hbutton.style.display = "none";
+   snowStorm.stop();
+}
+</script>
+'; ?>
